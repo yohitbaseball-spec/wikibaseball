08ea5a5f28ef537b7e0aad0328fc356708a45d87
yohitbaseball-spec <yohit24@gmail.com>
2026-08-23 12:13:44 +0800

    Update update.py

--- update.py
+++ update.py
@@ -27,38 +27,48 @@ def outs_to_ip_str(outs):
     if f == 0: return f"{i}"
     else: return f"{i}.{f}"
 
-def parse_val(raw_val):
+def parse_num_and_star(raw_val):
+    """
+    解析字串，回傳 (純數字 float, 星號標記種類)
+    star_type: '' (無星號), '*' (單星號), '**' (雙星號)
+    """
     s = str(raw_val).strip()
+    if not s or s == '-':
+        return 0.0, ''
+    
+    star_type = ''
+    clean_s = s
     if s.endswith('**'):
+        star_type = '**'
         clean_s = s[:-2].strip()
-        try: num = float(clean_s)
-        except: num = 0.0
-        return num, f'<b style="color: red;">{clean_s}</b>'
     elif s.endswith('*'):
+        star_type = '*'
         clean_s = s[:-1].strip()
-        try: num = float(clean_s)
-        except: num = 0.0
-        return num, f'<b>{clean_s}</b>'
-    else:
-        try: num = float(s)
-        except: num = 0.0
-        return num, s
+        
+    try:
+        num = float(clean_s)
+    except:
+        num = 0.0
+        
+    return num, star_type
 
-def fmt_stat(raw_val, calc_num, precision=0):
-    raw_str = str(raw_val).strip()
-    if raw_str != "":
-        num, _ = parse_val(raw_str)
-        if precision == 3: formatted_num = f"{num:.3f}"
-        elif precision == 2: formatted_num = f"{num:.2f}"
-        else: formatted_num = f"{int(num)}" if num.is_integer() else f"{num}"
-            
-        if raw_str.endswith('**'): return f'<b style="color: red;">{formatted_num}</b>'
-        elif raw_str.endswith('*'): return f'<b>{formatted_num}</b>'
-        else: return formatted_num
+def format_cell(num, star_type, precision=0):
+    """
+    將數值與星號標記轉為 HTML 格式
+    """
+    if precision == 3:
+        formatted_num = f"{num:.3f}"
+    elif precision == 2:
+        formatted_num = f"{num:.2f}"
     else:
-        if precision == 3: return f"{calc_num:.3f}"
-        elif precision == 2: return f"{calc_num:.2f}"
-        else: return f"{int(calc_num)}" if calc_num.is_integer() else f"{calc_num}"
+        formatted_num = f"{int(num)}" if float(num).is_integer() else f"{num}"
+
+    if star_type == '**':
+        return f'<b style="color: red;">{formatted_num}</b>'
+    elif star_type == '*':
+        return f'<b>{formatted_num}</b>'
+    else:
+        return formatted_num
 
 def fetch_sheet_data(sheet_name):
     encoded_sheet_name = quote(sheet_name)
@@ -80,76 +90,84 @@ def process_batting_stats():
     for pid, group in df.groupby('player_id'):
         rows_html = ""
         
-        # 通算累計變數
-        tot_g = tot_pa = tot_ab = tot_h = tot_2b = tot_3b = tot_hr = 0
-        tot_rbi = tot_r = tot_sb = tot_tb = tot_so = tot_bb = tot_sac = tot_sf = 0
+        # 通算累計變數 (純數字加總)
+        tot_g = tot_pa = tot_ab = tot_h = tot_2b = tot_3b = tot_hr = 0.0
+        tot_rbi = tot_r = tot_sb = tot_tb = tot_so = tot_bb = tot_sac = tot_sf = 0.0
 
         for _, row in group.iterrows():
-            year_str = parse_val(row.get('year', ''))[1]
-            team_str = parse_val(row.get('team', ''))[1]
+            year_val, y_star = parse_num_and_star(row.get('year', ''))
+            year_str = format_cell(year_val, y_star, 0) if str(row.get('year', '')).strip() != '' else ''
+            team_str = str(row.get('team', '')).strip()
 
-            g, _ = parse_val(row.get('G', '0'))
-            pa, _ = parse_val(row.get('PA', '0'))
-            ab, _ = parse_val(row.get('AB', '0'))
-            h, _ = parse_val(row.get('H', '0'))
-            b2, _ = parse_val(row.get('2B', '0'))
-            b3, _ = parse_val(row.get('3B', '0'))
-            hr, _ = parse_val(row.get('HR', '0'))
-            rbi, _ = parse_val(row.get('RBI', '0'))
-            r, _ = parse_val(row.get('R', '0'))
-            sb, _ = parse_val(row.get('SB', '0'))
-            so, _ = parse_val(row.get('SO', '0'))
-            bb, _ = parse_val(row.get('BB', '0'))
-            sac, _ = parse_val(row.get('SAC', '0'))
-            sf, _ = parse_val(row.get('SF', '0'))
+            g, g_star = parse_num_and_star(row.get('G', '0'))
+            pa, pa_star = parse_num_and_star(row.get('PA', '0'))
+            ab, ab_star = parse_num_and_star(row.get('AB', '0'))
+            h, h_star = parse_num_and_star(row.get('H', '0'))
+            b2, b2_star = parse_num_and_star(row.get('2B', '0'))
+            b3, b3_star = parse_num_and_star(row.get('3B', '0'))
+            hr, hr_star = parse_num_and_star(row.get('HR', '0'))
+            rbi, rbi_star = parse_num_and_star(row.get('RBI', '0'))
+            r, r_star = parse_num_and_star(row.get('R', '0'))
+            sb, sb_star = parse_num_and_star(row.get('SB', '0'))
+            so, so_star = parse_num_and_star(row.get('SO', '0'))
+            bb, bb_star = parse_num_and_star(row.get('BB', '0'))
+            sac, sac_star = parse_num_and_star(row.get('SAC', '0'))
+            sf, sf_star = parse_num_and_star(row.get('SF', '0'))
 
             raw_tb = str(row.get('TB', '')).strip()
             if raw_tb != "":
-                tb, _ = parse_val(raw_tb)
+                tb, tb_star = parse_num_and_star(raw_tb)
             else:
                 tb = h + b2 + (2 * b3) + (3 * hr)
+                tb_star = ''
 
-            # 累加通算數據
+            # 累加通算數字
             tot_g += g; tot_pa += pa; tot_ab += ab; tot_h += h
             tot_2b += b2; tot_3b += b3; tot_hr += hr; tot_rbi += rbi
             tot_r += r; tot_sb += sb; tot_tb += tb; tot_so += so
             tot_bb += bb; tot_sac += sac; tot_sf += sf
 
+            # 計算當季數據
             avg_calc = (h / ab) if ab > 0 else 0.0
             obp_calc = ((h + bb) / (ab + bb + sf)) if (ab + bb + sf) > 0 else 0.0
             slg_calc = (tb / ab) if ab > 0 else 0.0
             ops_calc = obp_calc + slg_calc
 
-            avg_str = fmt_stat(row.get('AVG', ''), avg_calc, 3)
-            obp_str = fmt_stat(row.get('OBP', ''), obp_calc, 3)
-            slg_str = fmt_stat(row.get('SLG', ''), slg_calc, 3)
-            ops_str = fmt_stat(row.get('OPS', ''), ops_calc, 3)
+            avg_val, avg_star = parse_num_and_star(row.get('AVG', ''))
+            obp_val, obp_star = parse_num_and_star(row.get('OBP', ''))
+            slg_val, slg_star = parse_num_and_star(row.get('SLG', ''))
+            ops_val, ops_star = parse_num_and_star(row.get('OPS', ''))
+
+            avg_final = format_cell(avg_val if str(row.get('AVG', '')).strip() != '' else avg_calc, avg_star, 3)
+            obp_final = format_cell(obp_val if str(row.get('OBP', '')).strip() != '' else obp_calc, obp_star, 3)
+            slg_final = format_cell(slg_val if str(row.get('SLG', '')).strip() != '' else slg_calc, slg_star, 3)
+            ops_final = format_cell(ops_val if str(row.get('OPS', '')).strip() != '' else ops_calc, ops_star, 3)
 
             rows_html += f"""          <tr>
             <td>{year_str}</td>
             <td>{team_str}</td>
-            <td>{fmt_stat(row.get('G', ''), g)}</td>
-            <td>{fmt_stat(row.get('PA', ''), pa)}</td>
-            <td>{fmt_stat(row.get('AB', ''), ab)}</td>
-            <td>{fmt_stat(row.get('H', ''), h)}</td>
-            <td>{fmt_stat(row.get('2B', ''), b2)}</td>
-            <td>{fmt_stat(row.get('3B', ''), b3)}</td>
-            <td>{fmt_stat(row.get('HR', ''), hr)}</td>
-            <td>{fmt_stat(row.get('RBI', ''), rbi)}</td>
-            <td>{fmt_stat(row.get('R', ''), r)}</td>
-            <td>{fmt_stat(row.get('SB', ''), sb)}</td>
-            <td>{fmt_stat(row.get('TB', ''), tb)}</td>
-            <td>{fmt_stat(row.get('SO', ''), so)}</td>
-            <td>{fmt_stat(row.get('BB', ''), bb)}</td>
-            <td>{fmt_stat(row.get('SAC', ''), sac)}</td>
-            <td>{fmt_stat(row.get('SF', ''), sf)}</td>
-            <td>{avg_str}</td>
-            <td>{obp_str}</td>
-            <td>{slg_str}</td>
-            <td>{ops_str}</td>
+            <td>{format_cell(g, g_star)}</td>
+            <td>{format_cell(pa, pa_star)}</td>
+            <td>{format_cell(ab, ab_star)}</td>
+            <td>{format_cell(h, h_star)}</td>
+            <td>{format_cell(b2, b2_star)}</td>
+            <td>{format_cell(b3, b3_star)}</td>
+            <td>{format_cell(hr, hr_star)}</td>
+            <td>{format_cell(rbi, rbi_star)}</td>
+            <td>{format_cell(r, r_star)}</td>
+            <td>{format_cell(sb, sb_star)}</td>
+            <td>{format_cell(tb, tb_star)}</td>
+            <td>{format_cell(so, so_star)}</td>
+            <td>{format_cell(bb, bb_star)}</td>
+            <td>{format_cell(sac, sac_star)}</td>
+            <td>{format_cell(sf, sf_star)}</td>
+            <td>{avg_final}</td>
+            <td>{obp_final}</td>
+            <td>{slg_final}</td>
+            <td>{ops_final}</td>
           </tr>\n"""
 
-        # 計算通算成績（至少需有 1 個年度）
+        # 自動生成通算成績
         if len(group) > 0:
             tot_avg = (tot_h / tot_ab) if tot_ab > 0 else 0.0
             tot_obp = ((tot_h + tot_bb) / (tot_ab + tot_bb + tot_sf)) if (tot_ab + tot_bb + tot_sf) > 0 else 0.0
@@ -192,31 +210,33 @@ def process_pitching_stats():
     for pid, group in df.groupby('player_id'):
         rows_html = ""
         
-        tot_g = tot_gs = tot_w = tot_l = tot_hld = tot_sv = tot_outs = tot_qs = 0
-        tot_bf = tot_bh = tot_bhr = tot_so = tot_bb = tot_r = tot_er = 0
+        tot_g = tot_gs = tot_w = tot_l = tot_hld = tot_sv = tot_outs = tot_qs = 0.0
+        tot_bf = tot_bh = tot_bhr = tot_so = tot_bb = tot_r = tot_er = 0.0
 
         for _, row in group.iterrows():
-            year_str = parse_val(row.get('year', ''))[1]
-            team_str = parse_val(row.get('team', ''))[1]
+            year_val, y_star = parse_num_and_star(row.get('year', ''))
+            year_str = format_cell(year_val, y_star, 0) if str(row.get('year', '')).strip() != '' else ''
+            team_str = str(row.get('team', '')).strip()
 
-            ip_val = row.get('IP', '0')
-            outs = ip_to_outs(ip_val)
+            ip_val_raw = row.get('IP', '0')
+            ip_num, ip_star = parse_num_and_star(ip_val_raw)
+            outs = ip_to_outs(ip_val_raw)
             ip_actual = outs / 3.0
 
-            g, _ = parse_val(row.get('G', '0'))
-            gs, _ = parse_val(row.get('GS', '0'))
-            w, _ = parse_val(row.get('W', '0'))
-            l, _ = parse_val(row.get('L', '0'))
-            hld, _ = parse_val(row.get('HLD', '0'))
-            sv, _ = parse_val(row.get('SV', '0'))
-            qs, _ = parse_val(row.get('QS', '0'))
-            bf, _ = parse_val(row.get('BF', '0'))
-            bh, _ = parse_val(row.get('BH', '0'))
-            bhr, _ = parse_val(row.get('BHR', '0'))
-            so, _ = parse_val(row.get('SO', '0'))
-            bb, _ = parse_val(row.get('BB', '0'))
-            r, _ = parse_val(row.get('R', '0'))
-            er, _ = parse_val(row.get('ER', '0'))
+            g, g_star = parse_num_and_star(row.get('G', '0'))
+            gs, gs_star = parse_num_and_star(row.get('GS', '0'))
+            w, w_star = parse_num_and_star(row.get('W', '0'))
+            l, l_star = parse_num_and_star(row.get('L', '0'))
+            hld, hld_star = parse_num_and_star(row.get('HLD', '0'))
+            sv, sv_star = parse_num_and_star(row.get('SV', '0'))
+            qs, qs_star = parse_num_and_star(row.get('QS', '0'))
+            bf, bf_star = parse_num_and_star(row.get('BF', '0'))
+            bh, bh_star = parse_num_and_star(row.get('BH', '0'))
+            bhr, bhr_star = parse_num_and_star(row.get('BHR', '0'))
+            so, so_star = parse_num_and_star(row.get('SO', '0'))
+            bb, bb_star = parse_num_and_star(row.get('BB', '0'))
+            r, r_star = parse_num_and_star(row.get('R', '0'))
+            er, er_star = parse_num_and_star(row.get('ER', '0'))
 
             tot_g += g; tot_gs += gs; tot_w += w; tot_l += l
             tot_hld += hld; tot_sv += sv; tot_outs += outs; tot_qs += qs
@@ -228,31 +248,35 @@ def process_pitching_stats():
             era_calc = (er * 9.0) / ip_actual if ip_actual > 0 else 0.0
             whip_calc = (bb + bh) / ip_actual if ip_actual > 0 else 0.0
 
-            oavg_str = fmt_stat(row.get('OAVG', ''), oavg_calc, 3)
-            era_str = fmt_stat(row.get('ERA', ''), era_calc, 2)
-            whip_str = fmt_stat(row.get('WHIP', ''), whip_calc, 2)
+            oavg_val, oavg_star = parse_num_and_star(row.get('OAVG', ''))
+            era_val, era_star = parse_num_and_star(row.get('ERA', ''))
+            whip_val, whip_star = parse_num_and_star(row.get('WHIP', ''))
+
+            oavg_final = format_cell(oavg_val if str(row.get('OAVG', '')).strip() != '' else oavg_calc, oavg_star, 3)
+            era_final = format_cell(era_val if str(row.get('ERA', '')).strip() != '' else era_calc, era_star, 2)
+            whip_final = format_cell(whip_val if str(row.get('WHIP', '')).strip() != '' else whip_calc, whip_star, 2)
 
             rows_html += f"""          <tr>
             <td>{year_str}</td>
             <td>{team_str}</td>
-            <td>{fmt_stat(row.get('G', ''), g)}</td>
-            <td>{fmt_stat(row.get('GS', ''), gs)}</td>
-            <td>{fmt_stat(row.get('W', ''), w)}</td>
-            <td>{fmt_stat(row.get('L', ''), l)}</td>
-            <td>{fmt_stat(row.get('HLD', ''), hld)}</td>
-            <td>{fmt_stat(row.get('SV', ''), sv)}</td>
-            <td>{parse_val(ip_val)[1]}</td>
-            <td>{fmt_stat(row.get('QS', ''), qs)}</td>
-            <td>{fmt_stat(row.get('BF', ''), bf)}</td>
-            <td>{fmt_stat(row.get('BH', ''), bh)}</td>
-            <td>{fmt_stat(row.get('BHR', ''), bhr)}</td>
-            <td>{oavg_str}</td>
-            <td>{fmt_stat(row.get('SO', ''), so)}</td>
-            <td>{fmt_stat(row.get('BB', ''), bb)}</td>
-            <td>{fmt_stat(row.get('R', ''), r)}</td>
-            <td>{fmt_stat(row.get('ER', ''), er)}</td>
-            <td>{era_str}</td>
-            <td>{whip_str}</td>
+            <td>{format_cell(g, g_star)}</td>
+            <td>{format_cell(gs, gs_star)}</td>
+            <td>{format_cell(w, w_star)}</td>
+            <td>{format_cell(l, l_star)}</td>
+            <td>{format_cell(hld, hld_star)}</td>
+            <td>{format_cell(sv, sv_star)}</td>
+            <td>{format_cell(ip_num, ip_star, 1 if ip_num % 1 != 0 else 0)}</td>
+            <td>{format_cell(qs, qs_star)}</td>
+            <td>{format_cell(bf, bf_star)}</td>
+            <td>{format_cell(bh, bh_star)}</td>
+            <td>{format_cell(bhr, bhr_star)}</td>
+            <td>{oavg_final}</td>
+            <td>{format_cell(so, so_star)}</td>
+            <td>{format_cell(bb, bb_star)}</td>
+            <td>{format_cell(r, r_star)}</td>
+            <td>{format_cell(er, er_star)}</td>
+            <td>{era_final}</td>
+            <td>{whip_final}</td>
           </tr>\n"""
 
         if len(group) > 0:
